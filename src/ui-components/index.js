e10341ba67fdfe332023ead9405d56cab911d6b8
Fahn Péter <fahnp@datapress.hu>
2023-07-21 13:35:14 +0200

    Add UI components

---
+++ src/ui-components/index.js
@@ -0,0 +1,11 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+export { default as ActionCard } from "./ActionCard";
+export { default as ActionCardCollection } from "./ActionCardCollection";
+export { default as studioTheme } from "./studioTheme";
+export { default as HomeUpdateForm } from "./HomeUpdateForm";
+export { default as NewForm1 } from "./NewForm1";
